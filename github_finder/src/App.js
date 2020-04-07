4f97b93cdc269c56d70606a31ba51d952e5c85c4
PavelZaika <zpk.loki@gmail.com>
2020-04-07 15:52:55 +0300

    component state

--- github_finder/src/App.js
+++ github_finder/src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
+import UserItem from './components/users/UserItem';
 import Navbar from './components/layout/Navbar';
 
 class App extends Component {
   render() {
+    const NavBarTitle = 'GitHub Finder';
     return (
       <div className='App'>
-        <Navbar title='GitHub Finder' icon='fab fa-github' />
+        <Navbar title={NavBarTitle} icon='fab fa-github' />
+        <UserItem />
       </div>
     );
   }
